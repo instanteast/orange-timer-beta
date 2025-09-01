be63d41cb104008381568edca24f532640499928
instanteast <dyoon0312x@gmail.com>
2025-09-01 20:54:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -187,7 +187,7 @@ function updateDates() {
   const ddaymockDate = '2025-09-03';
   const dday2026Date = '2025-11-13';
 
-  document.getElementById('dday-mock').innerHTML = "수능 대박 기원!"+"<br>"+"오렌지t가 여러분을 응원합니다🍊";
+  document.getElementById('dday-mock').innerHTML = "수능 대박 기원! 오렌지t가 여러분을 응원합니다";
   document.getElementById('dday-2026').innerHTML = `<span class="highlight">26수능</span><br> [D-${calcDday(dday2026Date)}]`;
 
 }
@@ -296,6 +296,7 @@ document.addEventListener('keydown', resetUIHideTimer);
 document.addEventListener('DOMContentLoaded', () => {
   resetUIHideTimer();
 });
+
 
 
 
